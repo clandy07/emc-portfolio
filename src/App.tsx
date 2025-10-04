4e63024864523259efa0bb70c55a18abfc738a5c
Eduardo Cortes <edo@FIETO-stable.local>
2025-10-04 22:30:04 +0800

    feat: Added hash-based navigation

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,34 @@ import Home from './screens/Home';
 import About from './screens/About';
 import Skills from './screens/Skills';
 import Projects from './screens/Projects'; 
+import Contacts from './screens/Contacts';
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Home />
-      <About />
-      <Skills />
-      <Projects />
+    <Header />
+      <main className="snap-y snap-mandatory overflow-y-scroll scroll-smooth">
+      <section id="home" className="snap-start">
+        <Home />
+      </section>
+      <section id="about" className="snap-start">
+        <About />
+      </section>
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
+      <section id="contacts" className="snap-start">
+        <Contacts />
+      </section>
+    </main>
     </>
   )
 }
 
 export default App
+
+        
--- src/screens/Projects.tsx
+++ src/screens/Projects.tsx
@@ -2,14 +2,13 @@
 
 function Projects() {
     return (
-        <section className="py-12">
-            <h2 className="text-3xl font-bold text-center mb-8 text-black-800 dark:text-black">
-                Projects
-            </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8 px-6 max-w-6xl mx-auto">
-                {/* Project cards can be added here */}
-            </div>
-        </section>
+        <section className="scroll-smooth bg-white w-full min-h-screen text-black p-20">
+      <div className="flex justify-center min-h-screen">
+        <h1 className="letter text-3xl font-semibold text-[60px] text-black">
+          Projects
+        </h1>
+      </div>    
+    </section>
     )
 }
 
